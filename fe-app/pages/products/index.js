1b4f40c664d0af4965f58da104e13031972e76b5
Stefania Larisa Barabas <stefania.barabas@Alexandrus-MacBook-Pro.local>
2023-06-01 17:11:14 +0300

    Working on products page

--- fe-app/pages/products/index.js
+++ fe-app/pages/products/index.js
@@ -1,4 +1,10 @@
+import {useRouter} from "next/navigation"
+
 const Products = () => {
+    const token = localStorage.getItem('accessToken')
+    if(!token){
+        router.push('/404')
+    }
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <p>Products page</p>
