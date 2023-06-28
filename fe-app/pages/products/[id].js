cf39e801889bda1db883edeb902e35071a8375f3
Stefania Larisa Barabas <stefania.barabas@Alexandrus-MacBook-Pro.local>
2023-06-28 21:55:19 +0300

    Added product page and edit functionality

--- fe-app/pages/products/[id].js
+++ fe-app/pages/products/[id].js
@@ -1,25 +1,127 @@
 import {useRouter} from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Heading, FormControl, FormLabel, Input, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Box, Textarea, Button} from '@chakra-ui/react'
+import axios from 'axios'
+import { useToast } from '@chakra-ui/react'
 
-const ProductDetails = () => {
+export default function ProductDetails({id})  {
     const router = useRouter();
+    const toast = useToast()
+
+    const [product, setProduct] = useState({})
+    const [productName, setProductName] = useState("")
+    const [productDescription, setProductDescription] = useState("")
+    const [productQuantity, setProductQuantity] = useState("")
+
+
     let token;
+
     if (typeof window !== 'undefined') {
         // Perform localStorage action
         token = localStorage.getItem('accessToken')
-      }
-      console.log(token)
+    }
+
+    const handleProductName = (event) => {
+        setProductName(event.target.value)
+    }
+
+    const handleProductDescription = (event) => {
+        setProductDescription(event.target.value)
+    }
+
+    const handleProductQuantity = (valueString) => {
+        console.log(Number(valueString))
+        setProductQuantity(Number(valueString))
+    }
+
+
+    const getProductData = async () => {
+        const config = {
+            withCredentials: true,
+        };
+    
+        await axios.get(`http://localhost:3300/products/${id}`, config).then((response) => {
+            if(response.data){
+                setProduct(response.data)
+                setProductName(response.data.product_name)
+                setProductDescription(response.data.product_description)
+                setProductQuantity(response.data.product_quantity)
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    const saveProductData = async () => {
+        const config = {
+            withCredentials: true,
+        };
+        const data = {
+            product_name: productName,
+            product_description: productDescription,
+            product_quantity: productQuantity
+        }
+
+        axios.patch(`http://localhost:3300/products/${id}`, data, config)
+          .then(function (response) {
+            console.log(response);
+            getProductData()
+            toast({
+                title: 'Product updated',
+                description: "Your product has been updated",
+                status: 'success',
+                position: 'top-right',
+                duration: 5000,
+                isClosable: true,
+              })
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
 
       useEffect(()=>{
         if(!token){
             router.push('/login')
+        } else{
+            getProductData()
         }
       }, [token])
+      
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p>Product page</p>
+        <main className="min-h-screen p-24">
+            <Box backgroundColor='white' maxW='3xl' borderWidth='1px' borderRadius='lg' overflow='hidden' pt='20' pb="20" pl='10' pr='10'>
+                <Heading size="xl" mb='6'>{product.product_name}</Heading>
+                <FormControl mb="8">
+                    <FormLabel>Product name</FormLabel>
+                    <Input value={productName} onChange={handleProductName} type='text' />
+                </FormControl>
+                <FormControl mb="8">
+                <FormLabel>Quantity</FormLabel>
+                    <NumberInput value={productQuantity} onChange={handleProductQuantity} max={50} min={10}>
+                        <NumberInputField />
+                        <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                </FormControl>
+                <FormControl mb="8">
+                    <FormLabel>Description</FormLabel>
+                    <Textarea   
+                        value={productDescription}
+                        onChange={handleProductDescription}
+                        size='sm'
+                    >
+                    </Textarea>
+                </FormControl>
+                <Button variant='solid' colorScheme='blue' onClick={saveProductData}> Save</Button>
+            </Box>
         </main>
     )
 }
 
-export default ProductDetails
+ProductDetails.getInitialProps = ({ query: { id } }) => {
+    return { id }
+  }
+
--- fe-app/pages/products/index.js
+++ fe-app/pages/products/index.js
@@ -1,10 +1,15 @@
 
 import {useRouter} from "next/navigation"
-import { useEffect } from "react"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { Heading, Button, Card, CardBody, Stack, Text, Image, ButtonGroup, Divider, CardFooter, SimpleGrid } from '@chakra-ui/react'
 import axios from 'axios'
 
 const Products = () => {
+    console.log('page loaded')
     const router = useRouter();
+    const [products, setProducts] = useState([])
+    const [categories, setCategories] = useState([])
     let token;
     if (typeof window !== 'undefined') {
         // Perform localStorage action
@@ -17,6 +22,9 @@ const Products = () => {
         };
         await axios.get("http://localhost:3300/products", config).then((response) => {
             console.log(response)
+            if(response.data){
+                setProducts(response.data)
+            }
         }).catch((error) => {
             console.log(error)
         })
@@ -27,10 +35,20 @@ const Products = () => {
             withCredentials: true,
         };
         await axios.get("http://localhost:3300/categories", config).then((response) => {
-            console.log(response)
+            if(response.data){
+                setCategories(response.data)
+            }
         }).catch((error) => {
             console.log(error)
         })
+    }
+
+    const getProductCategoryName = (product_category_id) => {
+        const categoryItem = categories.find(category => category.id === product_category_id)
+    
+        if(categoryItem){
+            return categoryItem.category_name
+        }
     }
 
     useEffect(()=>{
@@ -43,12 +61,50 @@ const Products = () => {
     }, [token])
 
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
-            <p>Products page</p>
+        <main className="min-h-screen p-24">
+            <Heading size="xl" mb='6'>Products page</Heading>
+            <SimpleGrid columns={4} spacingX='40px' spacingY='20px'>
+                {products.map((product, key) => (
+            
+                        <Card maxW='sm' mb='6' key={key}>
+                            <CardBody>
+                                <Image
+                                    src='https://alloutco.com/wp-content/uploads/2020/12/product-placeholder.jpg'
+                                    alt='Green double couch with wooden legs'
+                                    borderRadius='lg'
+                                />
+                                <Stack mt='6' spacing='3'>
+                                    <Heading size='md'>{product.product_name}</Heading>
+                                    <Text>
+                                        {product.product_description}
+                                    </Text>
+                                    <Text color='grey.700' fontSize='md'>
+                                        Quantity: {product.product_quantity}
+                                    </Text>
+                                    <Text color='grey.700' fontSize='md'>
+                                        Category: {getProductCategoryName(product.category_id)}
+                                    </Text>
+                                </Stack>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                                <ButtonGroup spacing='2'>
+                                    <Button variant='outline' colorScheme='blue' onClick={() => router.push(`/products/${product.id}`)}>
+                                    Edit
+                                    </Button>
+                                    <Button variant='solid' colorScheme='red'>
+                                    Delete
+                                    </Button>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
+               
+                ))}
+            </SimpleGrid>
         </main>
     )
       
 }
-   
 
 export default Products;
+
